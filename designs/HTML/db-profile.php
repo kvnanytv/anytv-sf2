0ac8722da4a200bd084b2f273d6e6fc6d6fa05f6
kvn <kevin@any.tv>
2013-09-26 11:00:58 +0800

    09/26/13 11:00AM

--- designs/HTML/db-profile.php
+++ designs/HTML/db-profile.php
@@ -3,8 +3,8 @@
 <!-- content -->
 <section id="content">
 	<div class="container">
-		<div class="row-fluid">
-			<div class="col-lg-4">
+		<div class="row">
+			<div class="col-md-4">
 				<div id="user-db">
 					<div class="panel panel-default user-panel">
 					<div class="panel-heading"></div>
@@ -24,17 +24,28 @@
 								?>
 								</p>
 						</div>				
-						<div class="panel-body">
-							
+						<div class="panel-body">							
 							<div class="user-det">
 								<h3>Hi <span>Jane Doe</span>!</h3>
 								<p>Referral link: www.any.tv/sdf51h</p>
 							</div>
-
 						</div>
+					<div id="user-menus">
+						<ul class="nav nav-pills nav-stacked">
+						  <li class="active">
+						    <a href="#">
+						      <span class="badge pull-right">42</span>
+						      Dashboard
+						    </a>
+						  </li>
+						  <li><a href="#">My Profile</a></li>
+						  <li><a href="#">Offers</a></li>
+						  <li><a href="#">Reports</a></li>						  
+						</ul>
+					</div>
 					</div>
 
-					<div class="col-lg-8">
+					<div class="col-md-8">
 
 					</div>  
 				</div>
