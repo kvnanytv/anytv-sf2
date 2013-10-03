13cff73aec55471046a327dda828873c4428cbc0
kvn <kevin@any.tv>
2013-10-03 21:46:31 +0800

    UP

--- designs/HTML/db-offers.php
+++ designs/HTML/db-offers.php
@@ -81,7 +81,7 @@
 										<div class="input-group input-group-lg">
 											<input type="text" class="form-control input-lg" placeholder="Search Offers">
 											<span class="input-group-btn">
-												<button class="btn btn-default" type="button">Go!</button>
+												<button class="btn btn-default" type="button"><i class="icon-search"></i></button>
 											</span>
 										</div>
 										<div class="cl-20"></div>
