8ceafb66b62a734887c19da0c93677a406b9ebd9
kvn <kevin@any.tv>
2013-10-08 16:06:30 +0800

    UP

--- designs/HTML/sandbox.php
+++ designs/HTML/sandbox.php
@@ -1,48 +1,224 @@
-<div class="form-row-style">
-<div class="form_row">
-  <label for="company_company" class="required">Company</label>
-  <input type="text" id="company_company" name="company[company]" required="required" maxlength="255" value="AnyTV Philippines" />
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+  <title>Dashboard.tm | Part of any.tv family.</title>
+
+  <link rel="stylesheet" href="css/pword-strength/passwordStrength.css" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap-tagsinput/bootstrap-tagsinput.css">
+
+
+  <?php include 'header-dashboard.php'; ?>
+
+  <section id="content">
+    <div class="container">
+      <div id="con-body" class="row">
+        <div class="col-lg-12 al-mid">
+          <h3>Create an account today!</h3>
+          <div class="cl-20"></div>
+        </div>
+        <div class="col-lg-3"> </div>
+        <div class="col-lg-6">        
+          <div class="panel panel-default lgin-panel">
+            <div class="panel-body">
+              <form class="form-signin">
+                <!-- User Details -->
+                <div class="well">
+                  <!-- Email -->
+                  <div class="form_row">
+                    <div class="alert alert-danger">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>                
+                      Please fill up the field below.
+                    </div>              
+                    <p><strong>Email Address</strong> *</p>                 
+                    <input type="email" class="form-control input-lg brdrrad-0" placeholder="" autofocus>
+                  </div>                
+                  <div class="cl-20"></div>
+
+                  <!-- Password -->
+                  <div class="form_row">
+                    <div class="alert alert-danger">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>                
+                      Password did not match
+                    </div>                    
+                    <p style="float: left"><strong>Password</strong> *</p><span class="fld-lnk-sm"><label  for ="passwordStrength" class="help-inline" name="passwordStrengthLabel" id="passwordStrengthLabel" ></label></span>               
+                    <input id="passwordStrength" type="password" class="form-control input-lg passwordTarget control-group brdrrad-0">      
+                  </div>
+                  <div class="cl-20"></div>
+
+                  <!-- Confirm Password -->
+                  <div class="form_row">
+                    <p style="float: left"><strong>Confirm Password</strong> *</p>               
+                    <input type="password" class="form-control input-lg brdrrad-0">         
+                  </div>
+                  <div class="cl-20"></div>               
+
+                  <!-- First Name -->
+                  <div class="form_row">
+                    <p><strong>First name</strong> *</p>               
+                    <input type="text" class="form-control input-lg brdrrad-0">             
+                  </div>
+                  <div class="cl-20"></div>
+
+                  <!-- Last Name -->
+                  <div class="form_row">
+                    <p><strong>Last name</strong> *</p>               
+                    <input type="text" class="form-control input-lg brdrrad-0">  
+                  </div>       
+                  <div class="cl-20"></div> 
+
+                </div>
+
+                <!-- Account Details -->
+                <div class="well">
+
+                  <!-- Company Name -->
+                  <div class="form_row">
+                    <p><strong>Company Name</strong> *</p>
+                    <input type="email" class="form-control input-lg brdrrad-0" >
+                  </div>
+                  <div class="cl-20"></div>
+
+                  <!-- Address 1 -->
+                  <div class="form_row">
+                    <p><strong>Address 1</strong> *</p>               
+                    <input type="text" class="form-control input-lg brdrrad-0">
+                  </div>
+                  <div class="cl-20"></div>
+
+                  <!-- Address 2 -->
+                  <p><strong>Address 2</strong></p>               
+                  <input type="text" class="form-control input-lg brdrrad-0">
+                </div>         
+                <div class="cl-20"></div> 
+
+                <!-- City -->
+                <div class="form_row">
+                  <p><strong>City</strong> *</p>               
+                  <input type="text" class="form-control input-lg brdrrad-0">         
+                </div>
+                <div class="cl-20"></div>               
+
+                <!-- Country -->
+                <div class="form_row">
+                  <p><strong>Country</strong> *</p>
+                  <select class="selectpicker" data-live-search="true" data-width="100%">
+                    <option>Potato</option>
+                  </select>
+                </div>
+                <div class="cl-20"></div>
+
+                <div class="row">
+                  <div class="col-md-6">
+
+                    <!-- Zipcode -->
+                    <div class="form_row">
+                      <p><strong>Zipcode</strong> *</p>               
+                      <input type="text" class="form-control input-lg brdrrad-0">         
+                    </div>
+                    <div class="cl-20"></div>
+                  </div>
+                  <div class="col-md-6">
+                    <!-- Region -->
+                    <div class="form_row">
+                      <p><strong>Region</strong> *</p>               
+                      <input type="text" class="form-control input-lg brdrrad-0">         
+                    </div>
+                    <div class="cl-20"></div>
+                  </div>                                                              
+                </div>
+
+              </div>
+
+              <!-- Additional Questions -->
+              <div class="well">
+                <!-- Youtube Channels -->
+                <div class="form_row">
+                  <p><strong>Your YouTube Channels</strong></p>
+                  <input class="form-control" type="text" value="" name="yt-chs" data-role="tagsinput"/>
+                </div>
+                <div class="cl-20"></div>
+
+                <!-- Best Video -->
+                <div class="form_row">
+                  <p><strong>Link to your best video?</strong></p>
+                  <input class="form-control input-lg brdrrad-0" type="text" />
+                </div>
+                <div class="cl-20"></div>                 
+
+                <!-- YouTube Network -->
+                <div class="form_row">
+                  <p><strong>YouTube Network</strong></p>
+                  <input class="form-control input-lg brdrrad-0" type="text" />
+                </div>
+                <div class="cl-20"></div>
+
+                <!-- Twitch -->
+                <div class="form_row">
+                  <p><strong>Do you use Twitch or live stream?</strong></p>
+                  <div class="row">
+                    <div class="col-lg-2">                    
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                          Yes
+                        </label>
+                      </div>
+                    </div>
+                    <div class="col-lg-2">                    
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+                          No
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="cl-20"></div>
+
+                <!-- Skype Name -->
+                <div class="form_row">
+                  <p><strong>Skype Name</strong></p>
+                  <input class="form-control input-lg brdrrad-0" type="text" />
+                </div>
+                <div class="cl-20"></div>                 
+
+              </div>              
+
+              <div class="brdr-h"></div>    
+              <!-- Terms and Condition -->
+              <div class="form_row">
+                <div class="term-con">
+                  <input type="checkbox" value="">I agree to the <a href="" target="_blank">Terms and Conditions.</a>
+                </div>
+              </div>
+
+              <!-- Create Account Button -->
+              <div class="form_row">
+                <a class="btn btn-lg btn-primary btn-block" href="#"><strong>CREATE ACCOUNT</strong></a>
+              </div>
+            </div>                  
+          </div>
+        </div>
+        <div class="col-lg-3"></div>      
+
+    <!-- <div class="col-lg-5">       
+  </div> -->
+
+    <!-- <div class="col-lg-7 ft-game">       
+  </div> -->
+
 </div>
-
-<div class="form_row">
-  <label for="company_address1">Address1</label>
-  <input type="text" id="company_address1" name="company[address1]" maxlength="255" value="1902 Belmira, Cypress Towers" />
 </div>
+</section>       
+<!-- /content -->
 
-<div class="form_row"><label for="company_address2">Address2</label>
-  <input type="text" id="company_address2" name="company[address2]" maxlength="255" />
-</div>
-
-<div class="form_row"><label for="company_city">City</label>
-  <input type="text" id="company_city" name="company[city]" maxlength="255" value="Taguig City" />
-</div>
-
-<div class="form_row"><label for="company_region">Region</label>
-  <input type="text" id="company_region" name="company[region]" maxlength="255" value="NCR" />
-</div>
-
-<div class="form_row"><label for="company_country">Country</label>
-  <select id="company_country" name="company[country]">
-    <option value=""></option>
-    <option value="1">Afghanistan</option>
-    <option value="2">Aland Islands</option></select>
-  </div>
-
-  <img src="/app_dev.php/media/cache/profile_thumbnail/uploads/affiliates/thumbnails/beaca476948deea6fb0239ffb791e809675d2a44.jpeg" alt="AnyTV Philippines" />     
-  <div class="form_row"><label for="company_file">File</label>
-    <input type="file" id="company_file" name="company[file]" />
-  </div>
-
-  <div class="form_row">
-    <label for="company_phone">Phone</label>
-    <input type="text" id="company_phone" name="company[phone]" maxlength="255" value="639178014866" />
-  </div>
-
-  <div class="form_row">
-    <label for="company_fax">Fax</label>
-    <input type="text" id="company_fax" name="company[fax]" maxlength="255" value="027253202" />
-  </div>
-
-  <div><button type="submit" id="company_save" name="company[save]">Save</button></div><!-- 
-  <input type="hidden" id="company__token" name="company[_token]" value="75400fcd90e5f2c1c1e4e286cf076bdb72fc412e" /></form> -->
-</div>
+<?php include 'footer-dbpro.php' ?>
