d416fcead99b2fc7b0b39728b0366f3eda1400c5
kvn <kevin@any.tv>
2013-10-10 14:25:05 +0800

    UP

--- designs/HTML/sandbox.php
+++ designs/HTML/sandbox.php
@@ -1,14 +1,32 @@
-<style type="text/css">
-	div {
-		  height: 100px;
-  background-color: #ececec;
-  -webkit-animation: rotateFade 800ms ease-out;
-  -moz-animation: rotateFade 800ms ease-out;
-  -o-animation: rotateFade 800ms ease-out;
-  -ms-animation: rotateFade 800ms ease-out;
-  animation: rotateFade 800ms ease-out;
-	}
-</style>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>AnyTV | Frequently Asked Questions</title>
 
 
-<div></div>
+	<?php include 'header-main.php'; ?>
+
+	<div id="content">
+		<section class="sec-content-title">
+			<div class="container">
+				<h3>Frequently Asked Questions</h3>
+			</div>			
+		</section>
+
+<section class="sec-content">
+	<div class="container">
+	
+	</div>
+</section>		
+
+</div>
+</div>
+
+<?php include 'footer.php'; ?>
