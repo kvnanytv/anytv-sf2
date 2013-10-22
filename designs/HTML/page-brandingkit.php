6b530feafa399f9039e6207c2f336c5dc09e80c4
kvnanytv <anytvdev@George-Vanouss-MacBook-Pro-2.local>
2013-10-22 12:55:28 +0800

    UP

--- designs/HTML/page-brandingkit.php
+++ designs/HTML/page-brandingkit.php
@@ -12,36 +12,37 @@
 	<title>Dashboard.tm | Part of any.tv family.</title>
 
 	<?php include 'header-main.php'; ?>
+	<div id="content">
+		<section class="sec-content-title">
+			<div class="container">
+				<h3>Branding Kit</h3>
+			</div>			
+		</section>
 
-	<section class="sec-content-title">
-		<div class="container">
-			<h3>Branding Kit</h3>
-		</div>			
-	</section>
-
-	<section id="brndkt-slide1">
-		<div class="container">
-			<div class="row">
-				<h1>Download our Branding Kit</h1>
+		<section id="brndkt-slide1">
+			<div class="container">
 				<div class="row">
-					<div class="col-lg-2"></div>
-					<div class="col-lg-8">
-					<select class="selectpicker sp-lg" data-live-search="true">
-						<option>720p</option>
-					</select>
-					<button type="button" class="btn btn-lg">Download</button>
+					<h1>Download our Branding Kit</h1>
+					<div class="row">
+						<div class="col-lg-2"></div>
+						<div class="col-lg-8">
+							<select class="selectpicker sp-lg" data-live-search="true">
+								<option>720p</option>
+							</select>
+							<button type="button" class="btn btn-lg">Download</button>
+						</div>
+						<div class="col-lg-2"></div>
 					</div>
-					<div class="col-lg-2"></div>
+
 				</div>
-
 			</div>
-		</div>
-	</section>
-</div>
+		</section>
+	</div>
 
 
 
 
 
 
-<?php include 'footer-dbpro.php' ?>
+
+	<?php include 'footer-dbpro.php' ?>
