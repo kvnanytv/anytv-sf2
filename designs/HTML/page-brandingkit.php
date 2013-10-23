40a651121afe1310c2bfaab869edb877eb85564d
kvnanytv <kevin@any.tv>
2013-10-23 16:00:29 +0800

    UP

--- designs/HTML/page-brandingkit.php
+++ designs/HTML/page-brandingkit.php
@@ -11,8 +11,13 @@
 
 	<title>Dashboard.tm | Part of any.tv family.</title>
 
+	<style>
+		@import url('css/sandbox.css');
+	</style>
 	<?php include 'header-main.php'; ?>
+
 	<div id="content">
+
 		<section class="sec-content-title">
 			<div class="container">
 				<h3>Branding Kit</h3>
@@ -23,25 +28,46 @@
 			<div class="container">
 				<div class="row">
 					<h1>Download our Branding Kit</h1>
-					<div class="row">
-						<div class="col-lg-2"></div>
-						<div class="col-lg-8">
-							<select class="selectpicker" data-width="100%">
-								<option>720p</option>
-							</select>
-							<button type="button" class="btn btn-lg">Download</button>
-						</div>
-						<div class="col-lg-2"></div>
-					</div>
 
+					<div class="col-lg-2"></div>
+					<div class="col-lg-4">
+						<label>
+							<select>
+								<option selected> 720p (Full) </option>
+								<option>1080p (Full)</option>
+								<option>any.TV Intro</option>									
+								<option>any.TV Watermark</option>									
+								<option>any.TV Logos</option>									
+								<option>any.TV Videobar</option>									
+							</select>
+						</label>
+
+					</div>
+					<div class="col-lg-4"><button type="button" class="btn btn-lg btn-success"><i class="icon-download icon-large"></i> Download</button></div>
+					<div class="col-lg-2"></div>
 				</div>
 			</div>
 		</section>
-	</div>
 
+		<section id="brndkt-slide2">
+			<div class="container">
+				<div class="row">
+								<div class="video-container">
+								         <iframe src="http://www.youtube.com/embed/UCqJAZuvUS0&wmode=opaque" frameborder="0" width="560" height="315"></iframe>
+								</div>												
+					<div class="col-lg-6">
+						<h1>Re-upload the "What is any.TV" video to your channel to help your audience learn about any.TV! </h1>
+						<p>(Yes, you may claim it, monetize it and puts ads around the video if you want to :-)) Encourage them to sign-up using your "Refer-a-Friend" link in the video description. </p>
+					</div>
 
+					<div class="col-lg-6">
+						<h1>Why? When they do, any.TV pays YOU a lifetime 10% bonus on top of everything they make!</h1> <p>Note: When you add the "any.TV intro" to your videos, also add the following in your video description:</p>
+					</div>
+				</div>
+			</div>
+		</section>		
 
-
+	</div><!-- /#content -->
 
 
 
